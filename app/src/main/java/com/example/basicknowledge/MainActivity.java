6456211c5dbef7cef1de7f2c54776f590f9a07f2
shan.le <shan.le@ecarx.com.cn>
2020-10-14 14:39:08 +0800

    feat:Main界面列表基础事件
    Signed-off-by: shan.le <shan.le@ecarx.com.cn>

--- app/src/main/java/com/example/basicknowledge/MainActivity.java
+++ app/src/main/java/com/example/basicknowledge/MainActivity.java
@@ -2,27 +2,95 @@ package com.example.basicknowledge;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.basicknowledge.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends BaseActivity {
 
     private  final String TAG =getClass().getSimpleName();
+    private RecyclerView rvAddNewView;
+    private AddDataAdapter addDataAdapter;
+    private List<String> addViewed = new ArrayList<>(16);
+    private List<String> data = new ArrayList<>(16);
+    int i = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_dialog);
+        setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
+        initView();
         initData();
+        setView();
+        bindEvent();
+    }
+
+    private void bindEvent() {
+        addDataAdapter.setAddDataListener(new AddDataAdapter.addDataListener() {
+            @Override
+            public void onAddDataListener(int position) {
+                if(i < data.size()){
+                    addViewed.add(data.get(i));
+                }else {
+                    addViewed.add("【下一页5】" + i);
+                }
+                addDataAdapter.notifyDataSetChanged();
+                i++;
+            }
+        });
+        addDataAdapter.setLongClickListenerRemove(new AddDataAdapter.longClickListenerRemove() {
+            @Override
+            public void setLongClickListener(View view) {
+                addViewed.remove(rvAddNewView.getChildLayoutPosition(view));
+                addDataAdapter.notifyDataSetChanged();
+            }
+        });
+        addDataAdapter.setOnItemClickListener(new AddDataAdapter.OnItemClickListener() {
+            @Override
+            public void setOnItemClickListener(View view, String str) {
+                switch (str){
+                    case "CanvasActivity":
+//                        startActivity(new Intent(MainActivity.this,CanvasActivity.class));
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this,"item 未实现",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setView() {
+        GridLayoutManager linearLayoutManager = new GridLayoutManager(this, 4);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvAddNewView.setLayoutManager(linearLayoutManager);
+        addDataAdapter = new AddDataAdapter(addViewed);
+        //设置item间隔
+        rvAddNewView.addItemDecoration(new RecyclerItemDecoration(20,4));
+        rvAddNewView.setAdapter(addDataAdapter);
+    }
+
+    private void initView() {
+        rvAddNewView = findViewById(R.id.rv_main);
     }
 
     private void initData() {
+        data.add("CanvasActivity");
+    }
+
+   /* private void showDialog() {
         final CommonDialog dialog = new CommonDialog(MainActivity.this);
         dialog.setMessage("这是一个自定义Dialog。")
                 .setImageResId(R.mipmap.ic_launcher)
@@ -32,6 +100,7 @@ public class MainActivity extends BaseActivity {
             public void onPositiveClick() {
                 dialog.dismiss();
                 Toast.makeText(MainActivity.this,"xxxx",Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -40,5 +109,5 @@ public class MainActivity extends BaseActivity {
                 Toast.makeText(MainActivity.this,"ssss",Toast.LENGTH_SHORT).show();
             }
         }).show();
-    }
+    }*/
 }
